b96a7f7e8285e130afc8f1ecff018c71e0431d7b
alexmeirelles <alemeirelles@msn.com>
2022-03-16 18:14:17 -0300

    atualizacao pokedex

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+
+const pokedex = [
+    {
+        id: 1,
+        nome: "Bulbasaur",
+        descricao:"There is a plant seed on its back right from the day this Pokémon is born. The seed slowly grows larger.",
+        tipo: "Grass",
+        imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png"
+    },
+    {
+        id: 2,
+        nome: "Charmander",
+        descricao:"It has a preference for hot things. When it rains, steam is said to spout from the tip of its tail.",
+        tipo: "Fire",
+        imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png",
+    },
+    {
+        id: 3,
+        nome: "Squirtle",
+        descricao:"When it retracts its long neck into its shell, it squirts out water with vigorous force",
+        tipo: "Water",
+        imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png",
+    }
+]
+
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index", pokedex);
+});
+
+app.get("/home", (req, res) => {
+  res.send("Ola mundo do express");
+});
+
+app.listen(3000, () =>
+  console.log(" Servidor rodando em http://localhost:3000 ")
+);
